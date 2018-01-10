4fb7017e410d7a42a73cfd78538671f7b80159e7
armin.donnerstag <nick.kupka@gmail.com>
2018-01-10 15:04:50 +0100

    commit

--- src/mainpackage/c_mainClass.java
+++ src/mainpackage/c_mainClass.java
@@ -8,5 +8,6 @@ public class c_mainClass {
 		System.out.println("Hallo Alex kannst du das lesen?");
 		System.out.println("Jawohl, kann ich. Und commiten kann ich auch!");
 		System.out.println("La le lu");
+		System.out.println("tra la la la la");
 	}
 }
