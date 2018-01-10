88e3a1e1d2b2486f3238e2033e66ec16ebe4fe00
alex.freitag <nick.kupka@gmail.com>
2018-01-10 14:51:05 +0100

    Neuer sysout, viel Spaß damit

--- src/mainpackage/c_mainClass.java
+++ src/mainpackage/c_mainClass.java
@@ -6,5 +6,6 @@ public class c_mainClass {
 		System.out.println("Hello World!");
 		System.out.println("Second change for a commit.");
 		System.out.println("Hallo Alex kannst du das lesen?");
+		System.out.println("Jawohl, kann ich. Und commiten kann ich auch!");
 	}
 }
