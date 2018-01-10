5d77a8de5cc9ca47ab47983a7248de9a7b06a09a
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 14:46:01 +0100

    next commit alex kannst du das lesen

--- src/mainpackage/c_mainClass.java
+++ src/mainpackage/c_mainClass.java
@@ -5,5 +5,6 @@ public class c_mainClass {
 	public static void main (String[]args){
 		System.out.println("Hello World!");
 		System.out.println("Second change for a commit.");
+		System.out.println("Hallo Alex kannst du das lesen?");
 	}
 }
